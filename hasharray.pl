8a71a841b4defb5f473a27caf3eac40749ec421e
Quentin Delhaye <parastuffs@gmail.com>
2021-07-02 11:20:39 +0200

    Code example showing how to handle a hash of arrays.

---
+++ hasharray.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+# use 5.010;
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+my %hash = ();
+my $key = "mykey";
+my $value = "quick";
+
+# Square brackets to tell it's an array.
+$hash{$key} = [$value];
+
+print_hash();
+
+$value = "brown";
+
+# Push new var at the end of the array,
+# again dereferencing the array.
+push(@{ $hash{$key} }, $value);
+
+print_hash();
+
+# Extract the first element from the array.
+my $extract = shift(@{$hash{$key}});
+
+print "Extracted: $extract\n";
+
+print_hash();
+
+sub print_hash {
+
+	# $_ being the running variable, 
+	# we dereference the array from the  hash through @{...}
+	foreach (keys %hash) {
+	    print "$_: @{$hash{$_}}\n"; 
+	}
+}
